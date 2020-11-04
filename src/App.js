bca77cd21b06f69a6f2267797cef523c99176abf
Irma Blanco <irblancofdz@gmail.com>
2020-11-04 15:32:29 +0100

    Set up google directions

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MapWithADirectionsRenderer from "./MapWithADirectionsRenderer";
+
+function App() {
+  return (
+    <div className="App">
+      <MapWithADirectionsRenderer />;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MapWithADirectionsRenderer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+const { compose, withProps, lifecycle } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  DirectionsRenderer,
+} = require("react-google-maps");
+
+const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`;
+
+const MapWithADirectionsRenderer = compose(
+  withProps({
+    googleMapURL: googleMapsUrl,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap,
+  lifecycle({
+    componentDidMount() {
+      const DirectionsService = new window.google.maps.DirectionsService();
+
+      DirectionsService.route(
+        {
+          origin: new window.google.maps.LatLng(41.85073, -87.65126),
+          destination: new window.google.maps.LatLng(41.85258, -87.65141),
+          travelMode: window.google.maps.TravelMode.DRIVING,
+        },
+        (result, status) => {
+          if (status === window.google.maps.DirectionsStatus.OK) {
+            this.setState({
+              directions: result,
+            });
+          } else {
+            console.error(`error fetching directions ${result}`);
+          }
+        }
+      );
+    },
+  })
+)((props) => (
+  <GoogleMap
+    defaultZoom={7}
+    defaultCenter={new window.google.maps.LatLng(41.85073, -87.65126)}
+  >
+    {props.directions && <DirectionsRenderer directions={props.directions} />}
+  </GoogleMap>
+));
+
+export default MapWithADirectionsRenderer;
